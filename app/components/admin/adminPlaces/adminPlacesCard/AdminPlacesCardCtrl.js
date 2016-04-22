5ec10cf2a219bcdec1e404aba82cd09edcf58348
Gautier Delorme <gautierdelorme@users.noreply.github.com>
2016-04-22 15:26:55 +0200

    add remove place

--- app/components/admin/adminPlaces/adminPlacesCard/AdminPlacesCardCtrl.js
+++ app/components/admin/adminPlaces/adminPlacesCard/AdminPlacesCardCtrl.js
@@ -1,11 +1,23 @@
 angular.module('webapp').controller('AdminPlacesCardCtrl', AdminPlacesCardCtrl)
-AdminPlacesCardCtrl.$inject = []
+AdminPlacesCardCtrl.$inject = ['PlacesService', '$mdDialog']
 
 /**
  * @ngdoc controller
  * @name webapp.controller:AdminPlacesCardCtrl
  * @description In charge of the admin places card view.
  */
-function AdminPlacesCardCtrl() {
+function AdminPlacesCardCtrl(PlacesService, $mdDialog) {
     var vm = this
+
+    vm.removePlace = function(event) {
+        var confirm = $mdDialog.confirm()
+            .title('Would you like to delete your place?')
+            .ariaLabel('Lucky day')
+            .targetEvent(event)
+            .ok('Yes remove!')
+            .cancel('X')
+        $mdDialog.show(confirm).then(function() {
+            PlacesService.removePlace(vm.place)
+        })
+    }
 }
