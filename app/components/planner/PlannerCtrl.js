a86f3eaad775b5c6a48001993a823a8b1bd8813d
Gautier Delorme <gautierdelorme@users.noreply.github.com>
2016-04-28 10:26:11 +0200

    update planner

--- app/components/planner/PlannerCtrl.js
+++ app/components/planner/PlannerCtrl.js
@@ -1,13 +1,15 @@
 angular.module('webapp').controller('PlannerCtrl', PlannerCtrl)
-PlannerCtrl.$inject = []
+PlannerCtrl.$inject = ['SessionsService']
 
 /**
  * @ngdoc controller
  * @name webapp.controller:PlannerCtrl
  * @description In charge of the planner view.
  */
-function PlannerCtrl() {
+function PlannerCtrl(SessionsService) {
     var vm = this
 
     vm.titleName = "Planner"
+
+    vm.sessions = SessionsService.sessions
 }
