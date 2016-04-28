4341117d5ac3e5a3d4916ee9e003e5dd8cb2a911
Gautier Delorme <gautierdelorme@users.noreply.github.com>
2016-04-28 16:51:37 +0200

    update planner

--- app/components/planner/PlannerCtrl.js
+++ app/components/planner/PlannerCtrl.js
@@ -13,16 +13,31 @@ function PlannerCtrl(SessionsService, EventService, moment) {
 
     vm.selectedIndex = 0
 
+    vm.timeslots = []
+
     vm.days = []
 
     EventService.loadEvent().then(function() {
-        var start = moment(EventService.event.start_date)
-        var end = moment(EventService.event.end_date)
-        while (start.isSameOrBefore(end)) {
-            vm.days.push(angular.copy(start))
-            start.add(1, 'days')
+        if (EventService.event.start_date && EventService.event.end_date) {
+            var start = moment(EventService.event.start_date)
+            var end = moment(EventService.event.end_date)
+            while (start.isSameOrBefore(end)) {
+                vm.days.push(angular.copy(start))
+                start.add(1, 'days')
+            }
         }
+
+        if (EventService.event.timeslots) {
+            EventService.event.timeslots.forEach(function(item) {
+                vm.timeslots.push({
+                    start_time: new Date(item.start_time),
+                    end_time: new Date(item.end_time)
+                })
+            })
+        }
+
         vm.getSessions(0)
+
     })
 
     vm.selectDay = function(index) {
@@ -31,8 +46,8 @@ function PlannerCtrl(SessionsService, EventService, moment) {
     }
 
     vm.getSessions = function(index) {
-      SessionsService.getSessionsByDate(vm.days[index]).then(function(sessions){
-        vm.sessions = sessions
-      })
+        SessionsService.getSessionsByDate(vm.days[index]).then(function(sessions) {
+            vm.sessions = sessions
+        })
     }
 }
