a0ff774bcbaea9ec2084ef85fbfb3632729f16f2
micheau-bastien <micheau0bastien@gmail.com>
2016-04-29 15:47:41 +0200

    SessionSearch : Mise en place de la liste des sessions et récupération des conférences correspondantes

--- app/components/sessionsSearch/SessionSearchCtrl.js
+++ app/components/sessionsSearch/SessionSearchCtrl.js
@@ -8,4 +8,14 @@ function SessionSearchCtrl(SessionsService, ConferencesService) {
   vm.sessionActivated = null
   vm.conferences = []
 
+  vm.$routerOnActivate = function(next) {
+      vm.sessions = SessionsService.sessions
+  }
+
+  vm.$selectSession = function(idSession){
+    vm.sessionActivated = SessionsService.getSessionById(idSession)
+    for (var i = 0; i < vm.sessionActivated.conferences.length; i++) {
+        vm.conferences.push(ConferencesService.getConferenceById(vm.sessionActivated.conferences[i]))
+    }
+  }
 }
