d8a58e7be405f9e294c24aa2842098df07020948
micheau-bastien <micheau0bastien@gmail.com>
2016-04-29 17:54:47 +0200

    Expandable cards

--- app/components/sessionsSearch/SessionSearchCtrl.js
+++ app/components/sessionsSearch/SessionSearchCtrl.js
@@ -12,10 +12,16 @@ function SessionSearchCtrl(SessionsService, ConferencesService) {
       vm.sessions = SessionsService.sessions
   }
 
-  vm.$selectSession = function(idSession){
-    vm.sessionActivated = SessionsService.getSessionById(idSession)
-    for (var i = 0; i < vm.sessionActivated.conferences.length; i++) {
-        vm.conferences.push(ConferencesService.getConferenceById(vm.sessionActivated.conferences[i]))
+  vm.selectSession = function(session) {
+    console.log(session)
+    vm.conferences = []
+    vm.sessionActivated = session
+    if(typeof session.conferences !== 'undefined' ){
+      for (var i = 0; i < session.conferences.length; i++) {
+          vm.conferences.push(ConferencesService.getConferenceById(vm.sessionActivated.conferences[i]))
+      }
     }
+    console.log(vm.conferences)
+    console.log(vm.sessionActivated)
   }
 }
