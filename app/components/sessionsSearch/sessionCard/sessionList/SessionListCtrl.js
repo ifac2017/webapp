2f3b46fbb6f9fb7893ce425fbd8071ec11150493
micheau-bastien <micheau0bastien@gmail.com>
2016-04-30 11:39:43 +0200

    Remove console.log

--- app/components/sessionsSearch/sessionCard/sessionList/SessionListCtrl.js
+++ app/components/sessionsSearch/sessionCard/sessionList/SessionListCtrl.js
@@ -3,7 +3,6 @@ SessionListCtrl.$inject = ['PlacesService']
 
 function SessionListCtrl(PlacesService) {
   var vm = this
-  console.log(vm.conferences);
 
   vm.getPlaceById = function(placeId) {
     return PlacesService.getPlaceById(placeId)
