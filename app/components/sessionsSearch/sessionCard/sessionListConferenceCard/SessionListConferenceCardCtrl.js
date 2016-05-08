536e5bc96424cdef84392442001d31f8be2803b7
micheau-bastien <micheau0bastien@gmail.com>
2016-05-08 12:37:34 +0200

    Design edit

--- app/components/sessionsSearch/sessionCard/sessionListConferenceCard/SessionListConferenceCardCtrl.js
+++ app/components/sessionsSearch/sessionCard/sessionListConferenceCard/SessionListConferenceCardCtrl.js
@@ -1,7 +1,7 @@
 angular.module('webapp').controller('SessionListConferenceCardCtrl', SessionListConferenceCardCtrl)
-SessionListConferenceCardCtrl.$inject = ['PlacesService']
+SessionListConferenceCardCtrl.$inject = ['PlacesService', 'NotificationsService', 'CurrentUser']
 
-function SessionListConferenceCardCtrl(PlacesService) {
+function SessionListConferenceCardCtrl(PlacesService, NotificationsService, CurrentUser) {
   var vm = this
 
   vm.getPlaceById = function(placeId) {
@@ -10,5 +10,14 @@ function SessionListConferenceCardCtrl(PlacesService) {
 
   vm.saveConf = function(confId) {
     console.log("Cliked")
+  }
+
+  vm.action = function() {
+    console.log(vm.conference);
+    CurrentUser.saveConference(vm.conference).then(function() {
+      NotificationsService.success("Conference saved!")
+    }).catch(function(){
+      NotificationsService.error("You must be logged to save a conference.")
+    })
   }
 }
